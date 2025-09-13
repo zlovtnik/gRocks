fcd5a1ff9a0a4a617d4d517a4cf1e027aa73c829
zlovtnik <racasantos@icloud.com>
2025-09-12 23:37:04 -0400

    Enhance logging and error handling in application and request handlers

--- gRocks/src/gRocks_app.erl
+++ gRocks/src/gRocks_app.erl
@@ -10,12 +10,19 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    logger:set_primary_config(level, info),
+    logger:info("Starting Cowboy server..."),
     Dispatch = cowboy_router:compile([
         {'_', [
-            {"/process", handlers, []}
+            {<<"/process">>, handlers, []}
         ]}
     ]),
-    {ok, _} = cowboy:start_clear(my_http, [{port, 8080}], #{env => #{dispatch => Dispatch}}),
+    case cowboy:start_clear(my_http, [{port, 8080}], #{env => #{dispatch => Dispatch}}) of
+        {ok, Pid} ->
+            logger:info("Cowboy started successfully with PID: ~p", [Pid]);
+        {error, Reason} ->
+            logger:error("Failed to start Cowboy: ~p", [Reason])
+    end,
     gRocks_sup:start_link().
 
 stop(_State) ->
--- gRocks/src/handlers.erl
+++ gRocks/src/handlers.erl
@@ -3,9 +3,13 @@
 -export([init/2, terminate/3]).
 
 init(Req, State) ->
-    case cowboy_req:method(Req) of
+    Method = cowboy_req:method(Req),
+    Path = cowboy_req:path(Req),
+    logger:info("Received ~s request to ~s", [Method, Path]),
+    case Method of
         <<"POST">> ->
             {ok, Body, Req2} = cowboy_req:read_body(Req),
+            logger:info("Request body: ~s", [Body]),
             Result = utils:bind(utils:parse_data(Body), fun(Nums) ->
                 utils:bind(utils:validate_data(Nums), fun(Valid) ->
                     utils:compute_average(Valid)
@@ -13,15 +17,18 @@ init(Req, State) ->
             end),
             case Result of
                 {ok, Avg} ->
+                    logger:info("Computed average: ~p", [Avg]),
                     Json = jsx:encode(#{<<"average">> => Avg}),
                     Req3 = cowboy_req:reply(200, #{<<"content-type">> => <<"application/json">>}, Json, Req2),
                     {ok, Req3, State};
                 {error, Reason} ->
+                    logger:info("Error: ~p", [Reason]),
                     Json = jsx:encode(#{<<"error">> => atom_to_binary(Reason, utf8)}),
                     Req3 = cowboy_req:reply(400, #{<<"content-type">> => <<"application/json">>}, Json, Req2),
                     {ok, Req3, State}
             end;
         _ ->
+            logger:info("Method not allowed: ~s", [Method]),
             Req2 = cowboy_req:reply(405, Req),
             {ok, Req2, State}
     end.
